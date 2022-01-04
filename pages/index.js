1ab97256437675d4d4a249bc280f39be5e66bb31
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2022-01-04 09:40:50 +0000

    Revert changes

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
-import { Page, Button } from "@shopify/polaris";
+import { Heading, Page, Button } from "@shopify/polaris";
 import { Context } from "@shopify/app-bridge-react";
 import { Redirect } from "@shopify/app-bridge/actions";
-import ResourceListWithProducts from "../components/ResourceList";
+// import ResourceListWithProducts from "../components/ResourceList";
 
-class Products extends React.Component {
-	static contextType = Context;
-	handleCreate = () => {
-		const app = this.context;
-		const redirect = Redirect.create(app);
-		redirect.dispatch(Redirect.Action.APP, "/create-product");
-	};
-	render() {
-		return (
-			<Page>
-				<ResourceListWithProducts />
-				<div className="right-align">
-					<Button onClick={this.handleCreate}>Create Product</Button>
-				</div>
-			</Page>
-		);
-	}
-}
 
-export default Products;
+const Index = () => (
+	<Page>
+		<Heading>Shopify app with Node and React 🎉</Heading>
+	</Page>
+);
+
+export default Index;
--- server/handlers/mutations/get-one-time-url.js
+++ server/handlers/mutations/get-one-time-url.js
@@ -2,33 +2,33 @@ import "isomorphic-fetch";
 import { gql } from "apollo-boost";
 
 export function ONETIME_CREATE(url) {
-  return gql`
-    mutation {
-      appPurchaseOneTimeCreate(
-        name: "test"
-        price: { amount: 10, currencyCode: USD }
-        returnUrl: "${url}"
-        test: true
-      ) {
-        userErrors {
-          field
-          message
-        }
-        confirmationUrl
-        appPurchaseOneTime {
-          id
-        }
-      }
-    }
-  `;
+	return gql`
+		mutation {
+			appPurchaseOneTimeCreate(
+				name: "test"
+				price: { amount: 10, currencyCode: USD }
+				returnUrl: "${url}"
+				test: true
+			) {
+				userErrors {
+					field
+					message
+				}
+				confirmationUrl
+				appPurchaseOneTime {
+					id
+				}
+			}
+		}
+	`;
 }
 
 export const getOneTimeUrl = async (ctx) => {
-  const { client } = ctx;
-  const confirmationUrl = await client
-    .mutate({
-      mutation: ONETIME_CREATE(process.env.HOST),
-    })
-    .then((response) => response.data.appPurchaseOneTimeCreate.confirmationUrl);
-  return ctx.redirect(confirmationUrl);
+	const { client } = ctx;
+	const confirmationUrl = await client
+		.mutate({
+			mutation: ONETIME_CREATE(process.env.HOST),
+		})
+		.then((response) => response.data.appPurchaseOneTimeCreate.confirmationUrl);
+	return ctx.redirect(confirmationUrl);
 };
